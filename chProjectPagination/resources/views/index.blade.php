2b0f6a9ff32005202066b49bad0a582d1cc35431
Gilberto Hideaki Matsunaga <60249940+gmatsuna@users.noreply.github.com>
2021-08-22 23:00:07 +0900

    bootstrap and UI working

---
+++ chProjectPagination/resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="{{asset('site/style.css')}}">
+
+    <style>
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Index page</h1>
+    <button class="btn btn-jg btn-outline-orange">chProject Start</button>
+    <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+    </div>
+    <script src="{{asset('site/jquery.js')}}"></script>
+    <script src="{{asset('site/bootstrap.js')}}"></script>
+</body>
+</html>
